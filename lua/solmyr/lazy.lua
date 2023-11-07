3abb42ab14d28b84c95606cb8cf337c916e7cecb
Alex Gladyshev <aglad.dev@gmail.com>
2023-11-07 22:26:25 +0300

    Add lsp

--- lua/solmyr/lazy.lua
+++ lua/solmyr/lazy.lua
@@ -40,4 +40,13 @@ require("lazy").setup({
 	{'theprimeagen/harpoon'},
 	{'mbbill/undotree'},
 	{'tpope/vim-fugitive'},
+--- Uncomment these if you want to manage LSP servers from neovim
+-- {'williamboman/mason.nvim'},
+-- {'williamboman/mason-lspconfig.nvim'},
+
+{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+{'neovim/nvim-lspconfig'},
+{'hrsh7th/cmp-nvim-lsp'},
+{'hrsh7th/nvim-cmp'},
+{'L3MON4D3/LuaSnip'},
 })
