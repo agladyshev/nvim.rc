8dc698df33904eb72be42b4e3401009389ca535f
Alex Gladyshev <alex@lifter.ai>
2024-01-06 19:24:51 +0300

    Run debug ui without registered command

--- after/plugin/nvim-dap-ui.lua
+++ after/plugin/nvim-dap-ui.lua
@@ -1,4 +1,4 @@
 require('dapui').setup()
 
-vim.keymap.set("n", "<leader>dt", ":DapUiToggle<CR>", { noremap = true })
+vim.keymap.set("n", "<leader>dt", ":lua require('dapui').toggle()<CR>", { noremap = true })
 vim.keymap.set("n", "<leader>dr", ":lua require('dapui').open({reset = true})<CR>", { noremap = true })
